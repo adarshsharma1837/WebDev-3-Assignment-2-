21306c584202ea97cbe310e305b4dd64e360ac95
Adarsh Sharma <adisharma1837@gmail.com>
2026-09-19 22:43:03 +0530

    Delete WebDev-3 (Assignment- 2)/student-management-api directory

--- WebDev-3 (Assignment- 2)/student-management-api/data/students.js
+++
@@ -1,7 +0,0 @@
-let students = [
-  { id: 1, name: "Krithya Srivastava", course: "BTech" },
-  { id: 2, name: "Aditya Chauhan", course: "BTech" },
-  { id: 3, name: "Chirag Chanchal", course: "Gaming" }
-];
-
-module.exports = students;
--- WebDev-3 (Assignment- 2)/student-management-api/middleware/logger.js
+++
@@ -1,7 +0,0 @@
-const logger = (req, res, next) => {
-  const time = new Date().toISOString();
-  console.log(`[${time}] ${req.method} ${req.url}`);
-  next();
-};
-
-module.exports = logger;
